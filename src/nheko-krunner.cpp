bac083c0d52956d2c2ac04a6bc0a36d2eb37cc7f
Loren Burkholder <computersemiexpert@outlook.com>
2022-08-11 20:40:06 -0400

    Use the config option in the runner

--- src/nheko-krunner.cpp
+++ src/nheko-krunner.cpp
@@ -85,7 +85,7 @@ void NhekoKRunner::match(Plasma::RunnerContext &context)
         if (roomMatches)
         {
             auto text{room.roomName()};
-            if (room.unreadNotifications() > 0)
+            if (room.unreadNotifications() > 0 && m_showNotificationCounts)
                 text.append(QStringLiteral(" (%1)").arg(room.unreadNotifications()));
 
             Plasma::QueryMatch match{this};
@@ -168,9 +168,11 @@ void NhekoKRunner::reloadConfiguration()
     KConfigGroup conf = config();
 
     m_showNotificationCounts = conf.readEntry(QStringLiteral("showNotificationCounts"), true);
+    conf.writeEntry(QStringLiteral("showNotificationCounts"), m_showNotificationCounts);
+    conf.sync();
 }
 
-K_EXPORT_PLASMA_RUNNER_WITH_JSON(NhekoKRunner, "plasma-runner-nheko-krunner.json")
+K_PLUGIN_CLASS_WITH_JSON(NhekoKRunner, "plasma-runner-nheko-krunner.json")
 
 // needed for the QObject subclass declared as part of K_EXPORT_PLASMA_RUNNER_WITH_JSON
 #include "nheko-krunner.moc"
