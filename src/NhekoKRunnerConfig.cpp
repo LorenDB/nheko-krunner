469233c1aa3138e5c45a506ee6ad5e8c30eefb22
Loren Burkholder <computersemiexpert@outlook.com>
2022-08-11 20:36:34 -0400

    Fix the KCM

--- src/NhekoKRunnerConfig.cpp
+++ src/NhekoKRunnerConfig.cpp
@@ -2,11 +2,11 @@
 
 #include <KAboutData>
 #include <KPluginFactory>
+#include <KConfigGroup>
+#include <KSharedConfig>
 
 #include <QGridLayout>
-#include <QLabel>
-
-K_PLUGIN_FACTORY(NhekoKRunnerConfigFactory, registerPlugin<NhekoKRunnerConfig>();)
+#include <QLabel> // deleteme
 
 NhekoKRunnerConfig::NhekoKRunnerConfig(QWidget *parent, const QVariantList &args)
     : KCModule{parent, args}
@@ -15,15 +15,54 @@ NhekoKRunnerConfig::NhekoKRunnerConfig(QWidget *parent, const QVariantList &args
                                 QStringLiteral("nheko"),
                                 QStringLiteral("0.1.0"),
                                 tr("List rooms and perform other actions with nheko."),
-                                KAboutLicense::GPL_V3};
+                                KAboutLicense::LGPL_V2_1};
     about->addAuthor(QStringLiteral("Loren Burkholder"), tr("Author"), QStringLiteral("computersemiexpert@outlook.com"));
     setAboutData(about);
-
     setButtons(Apply);
 
     auto layout = new QGridLayout{this};
+    m_showNotificationCounts = new QCheckBox{tr("Display unread notification counts next to rooms")};
+    layout->addWidget(m_showNotificationCounts);
 
-    layout->addWidget(new QLabel{QStringLiteral("Nothing to see yet.")});
+    connect(m_showNotificationCounts, &QCheckBox::stateChanged, this, &NhekoKRunnerConfig::markAsChanged);
 }
+
+void NhekoKRunnerConfig::load()
+{
+    KCModule::load();
+
+    auto config = KSharedConfig::openConfig(QStringLiteral("krunnerrc"))
+            ->group(QStringLiteral("Runners"))
+                      .group(QStringLiteral("nheko-krunner"));
+
+    m_showNotificationCounts->setChecked(config.readEntry(QStringLiteral("showNotificationCounts"), true));
+
+    Q_EMIT changed(false);
+}
+
+void NhekoKRunnerConfig::save()
+{
+    KCModule::save();
+
+    auto config = KSharedConfig::openConfig(QStringLiteral("krunnerrc"))
+            ->group(QStringLiteral("Runners"))
+                      .group(QStringLiteral("nheko-krunner"));
+
+    config.writeEntry(QStringLiteral("showNotificationCounts"), m_showNotificationCounts->isChecked());
+    config.sync();
+
+    Q_EMIT changed(false);
+}
+
+void NhekoKRunnerConfig::defaults()
+{
+    KCModule::defaults();
+
+    m_showNotificationCounts->setChecked(true);
+
+    markAsChanged();
+}
+
+K_PLUGIN_CLASS(NhekoKRunnerConfig)
 
 #include "NhekoKRunnerConfig.moc"
--- src/NhekoKRunnerConfig.h
+++ src/NhekoKRunnerConfig.h
@@ -3,12 +3,22 @@
 
 #include <KCModule>
 
+#include <QObject>
+#include <QCheckBox>
+
 class NhekoKRunnerConfig : public KCModule
 {
     Q_OBJECT
 
 public:
     NhekoKRunnerConfig(QWidget *parent = nullptr, const QVariantList &args = {});
+
+    virtual void load() override;
+    virtual void save() override;
+    virtual void defaults() override;
+
+private:
+    QCheckBox *m_showNotificationCounts;
 };
 
 #endif // NHEKOKRUNNERCONFIG_H
